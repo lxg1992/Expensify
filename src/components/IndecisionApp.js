e7b9efad30ee3ba1e2c930f867a3af3a1f5f3aa2
Alexey Garin <lxg1992@gmail.com>
2020-01-13 12:04:28 -0500

    Styling options

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -81,6 +81,7 @@ export default class IndecisionApp extends React.Component {
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
           />
+          <div className="widget">
           <Options
             options={this.state.options}
             handleDeleteOptions={this.handleDeleteOptions}
@@ -89,6 +90,7 @@ export default class IndecisionApp extends React.Component {
           <AddOption
             handleAddOption={this.handleAddOption}
           />
+          </div>
         </div>
           <OptionModal handleClearModal={this.handleClearModal}selectedOption={this.state.selectedOption}/>
       </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,16 +4,21 @@ import Option from './Option'
 const Options = (props) => {
   return (
     <div>
+      <div className="widget-header">
+      <h3 className="widget-header__title">Your options</h3>
       <button 
         className="button button--link"
         onClick={props.handleDeleteOptions}
       >Remove All</button>
-      { props.options.length === 0 && <p>Add an option to get started</p> }
+      </div>
+
+      { props.options.length === 0 && <p className="widget-header__message">Add an option to get started</p> }
       {
-        props.options.map((option) => (
+        props.options.map((option, index) => (
         <Option 
           key={option} 
           optionText={option} 
+          count={index + 1}
           handleDeleteOption={props.handleDeleteOption}
         />)
         )
