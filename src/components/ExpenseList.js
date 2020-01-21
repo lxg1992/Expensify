efe3a5c91c12ccc861b58786d91b34a8f79d642a
Alex <lxg1992@gmail.com>
2020-01-21 00:37:22 -0500

    Mapping

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
 
 
 const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-        {props.expenses.length}
+        {props.expenses.map((expense) => (
+            <ExpenseListItem description={expense.description} createdAt={expense.createdAt} amount={expense.amount} />
+        ))}
     </div>
 );
 
---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default ({description, amount, createdAt}) => (
+    <div>
+        <h3>{description}</h3>
+        <p>{amount} - {createdAt}</p>
+    </div> 
+)
+
