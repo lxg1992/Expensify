6194a895756edc0d27528b2d5b6a9340d1c78570
Alexey <lxg1992@gmail.com>
2020-01-26 23:14:56 -0500

    Added Redux extension use + Editing and Creating new expenses

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,16 +3,21 @@ import moment from 'moment'
 import { SingleDatePicker } from 'react-dates'
 import 'react-dates/lib/css/_datepicker.css';
 
+
 const now = moment();
-console.log(now.format('MMM Do YYYY'))
 
 export default class ExpenseForm extends Component {
-    state = {
-        description: '',
-        note: '',
-        amount: '',
-        createdAt: moment(),
-        calendarFocused: false
+    constructor(props){
+        super(props)
+
+        this.state = {
+            description: props.expense ? props.expense.description : '',
+            note: props.expense ? props.expense.note : '',
+            amount: props.expense ? (props.expense.amount / 100).toString() : '',
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+            calendarFocused: false,
+            error: ''
+        }
     }
 
     onDescriptionChange= (e) => {
@@ -27,23 +32,43 @@ export default class ExpenseForm extends Component {
 
     onAmountChange = (e) => {
         const amount = e.target.value
-        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+        if (amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
             this.setState(() => ({amount}))
         }
     }
 
     onDateChange = (createdAt) => {
-        this.setState(() => ({createdAt}))
+        if(createdAt){
+            this.setState(() => ({createdAt}))
+        }
+
     }
 
     onFocusChange = ({focused}) => {
         this.setState(() => ({calendarFocused: focused}))
     }
 
+    onSubmit = (e) => {
+        e.preventDefault()
+        if(!this.state.description || !this.state.amount){
+            this.setState(() => ({error: 'Please provide description and amount'}))
+        } else {
+            this.setState(() => ({error: ''}))
+            this.props.onSubmit({
+                description: this.state.description,
+                amount: parseFloat(this.state.amount, 10) * 100,
+                createdAt: this.state.createdAt.valueOf(),
+                note: this.state.note
+
+            })
+        }
+    }
+
     render() {
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input
                         type="text"
                         placeholder="Description"
@@ -71,7 +96,8 @@ export default class ExpenseForm extends Component {
                         onChange={this.onNoteChange}
                     >
                     </textarea>
-                    <button>Add Expense</button>
+                    <button >{this.props.expense ? 'Save changes ' : 'Add expense'}</button>
+
                 </form>
             </div>
         )
