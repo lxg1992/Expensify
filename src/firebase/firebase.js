966be9753c898da417a0d80fd993a59bf72ca363
Alexey <lxg1992@gmail.com>
2020-02-05 11:24:09 -0500

    Setup test database environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,13 @@
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyBK_MFtXk2Vw0NFJIAw9fuxbXrCrgdZttA",
-  authDomain: "expense-tracker-d31b5.firebaseapp.com",
-  databaseURL: "https://expense-tracker-d31b5.firebaseio.com",
-  projectId: "expense-tracker-d31b5",
-  storageBucket: "expense-tracker-d31b5.appspot.com",
-  messagingSenderId: "1083310882855",
-  appId: "1:1083310882855:web:77126d91128d959e3b8e02",
-  measurementId: "G-57VVLFTG82"
+  apiKey:             process.env.FIREBASE_API_KEY,
+  authDomain:         process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL:        process.env.FIREBASE_DATABASE_URL,
+  projectId:          process.env.FIREBASE_PROJECT_ID,
+  storageBucket:      process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:              process.env.FIREBASE_APP_ID
 };
 
   // Initialize Firebase
