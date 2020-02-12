323caa90ec0019ef8470a2f298385b603ea33b82
Alexey <lxg1992@gmail.com>
2020-02-12 18:21:25 -0500

    Added Github and Google button CSS

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { startLogin, startGithubLogin } from '../actions/auth'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {  } from '@fortawesome/free-solid-svg-icons'
+import { faGoogle, faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 const LoginPage = (props) => {
     return(
@@ -8,8 +12,14 @@ const LoginPage = (props) => {
             <div className="box-layout__box">
                 <h1 className="box-layout__title">Expense Tracker</h1>
                 <p>Get your expenses in check</p>
-                <button onClick={props.startLogin}>Gmail Login</button>
-                <button onClick={props.startGithubLogin}>github login</button>
+            
+                <button className="box-layout__googleButton box-layout__button" onClick={props.startLogin}>
+                    <FontAwesomeIcon icon={faGoogle}/>
+                </button>
+                
+                <button className="box-layout__githubButton box-layout__button" onClick={props.startGithubLogin}>
+                    <FontAwesomeIcon icon={faGithub}/>
+                </button>
             </div>
         </div>
     )
